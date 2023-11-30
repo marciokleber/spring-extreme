e5be6f35018bd68125dcf0638300166bd8c96995
Marcio Kleber Junior Lopes de Souza <marciokjls@gmail.com>
2023-11-30 08:22:52 -0300

    Implementar web app angular-devextreme no projeto

--- src/main/java/lab/app/springextreme/SpringExtremeApplication.java
+++ src/main/java/lab/app/springextreme/SpringExtremeApplication.java
@@ -5,21 +5,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.DependsOn;
 
 @SpringBootApplication
 public class SpringExtremeApplication implements CommandLineRunner {
 
-	@Autowired
-	private Flyway flyway;
+    @Autowired
+    private Flyway flyway;
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringExtremeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExtremeApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		flyway.baseline();
-		flyway.migrate();
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        flyway.baseline();
+        flyway.migrate();
+    }
 }
